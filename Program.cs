13fe1c176827bfe3246ba0c9a7fcf882a9b24d0a
kuba82500 <kuba82500@gmail.com>
2019-02-24 18:37:20 +0100

    Cw2

--- Program.cs
+++ Program.cs
@@ -10,14 +10,77 @@ namespace csharp
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            ValueTypes();
+            
+            
+            for (; ; )
+            {
+                Greeting();
+                Age();
+                Settings();
+
+            }
+
+
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("maxInt = " + maxInt);
+            Console.WriteLine("minInt = " + minInt);
+            Console.WriteLine("minLong = " + minLong);
+            Console.WriteLine("maxLong = " + maxLong);
+        }
+
+        // <summary>
+        // Wita użytkownika po wprowadzeniu imienia
+        // </summary>
+        private static void Greeting()
+        {
+            Console.Write("Podaj swoje imię: ");
+            string name = Console.ReadLine();
             Console.WriteLine("Witaj " + name);
+        }
 
-            // foreach(var arg in args)
-            // {
-            //     System.Console.WriteLine("Witaj " + arg);
-            // }
+        // <summary>
+        // Wypisujemy dane wprowadzone przez użytkownika
+        // </summary>
+        private static void Age()
+        {
+            Console.Write("Wpisz ile masz lat: ");
 
+            int wiek;
+            bool result = int.TryParse(Console.ReadLine(), out wiek);
+
+            if(wiek >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Jesteś pełnoletni ! ");
+            }
+            else if (result == false)
+            {
+                System.Console.WriteLine("Wprowadziłeś znaki zamiast liczb");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Nie jesteś pełnoletni !");
+            }
+        }
+
+        // <summary>
+        //  Ustawienia 
+        // </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
         }
     }
 }
